0a784241c1000215681eb8f15aeba852b49a9dfd
Plintsr2 <148972014+Plintsr2@users.noreply.github.com>
2024-10-26 22:49:59 +0200

    add some new stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
+
+    static{
+        System.loadLibrary("libgitTest_JNI");
+    }
+   // before shit starts library needs to be raedy
+    //no exception
+
+
+
+
+    static {
+        System.out.println("im static block {1}");
+    }
+    {
+        System.out.println("{just block}");
+    }
+    static {
+        System.out.println("and im static block {2}");
+    }
     public static void main(String[] args) {
+        Main main = new Main();
+    System.out.println(main.DoSthWithInt(5));
 
 
     }
+    public native int DoSthWithInt(int iVal);
 }
